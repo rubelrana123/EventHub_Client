bbb992c812f710be7518e578f7c40cfa04693602
rubelrana123 <rubelranatpi707458@gmail.com>
2026-01-08 23:55:20 +0600

    vercel deploy issue solve

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -6,7 +6,7 @@ import InputFieldError from "./shared/InputFieldError";
 import { Button } from "./ui/button";
 import { Field, FieldDescription, FieldGroup, FieldLabel } from "./ui/field";
 import { Input } from "./ui/input";
-
+//login form component`
 const LoginForm = ({ redirect }: { redirect?: string }) => {
   const [state, formAction, isPending] = useActionState(loginUser, null);
   console.log("form login", state, formAction, isPending)
